0297f08ff40350ee08c08560dd81613c16c1cf81
dundalivre-star <dundalivre@gmail.com>
2026-08-22 09:21:56 +0100

    Remove all Prisma references

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import { prisma } from '@/src/lib/prisma'
 import bcrypt from 'bcryptjs'
 import { CustomSession, CustomJWT } from '@/types'
 
