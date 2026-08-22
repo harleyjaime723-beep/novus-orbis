3f9a0a6e349da19589836aaa390aac8daffa857b
dundalivre-star <dundalivre@gmail.com>
2026-08-22 08:20:31 +0100

    Fix prisma import

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import { prisma } from '@/lib/prisma'
+import { prisma } from '@/src/lib/prisma'
 import bcrypt from 'bcryptjs'
 import { CustomSession, CustomJWT } from '@/types'
 
